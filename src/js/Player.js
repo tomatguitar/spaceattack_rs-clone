e699c19d1d6dd60e5e07eb1e47db3d45819f0aab
Dmytro Zhyltsov <zkarman@mail.ru>
2021-01-12 21:02:21 +0200

    fix: fixed invalid player's class property name

--- src/js/Player.js
+++ src/js/Player.js
@@ -7,23 +7,27 @@ class Player extends Sprite {
     super(
       divName,
       position,
-      assetDesc.fileName,
-      new Size(assetDesc.width, assetDesc.heigth)
+      assetDesc.imgFileName,
+      new Size(assetDesc.width, assetDesc.height)
     );
     this.lives = SETTINGS.PLAYER.startLives;
     this.score = 0;
     this.highScore = 0;
-    this.state = SETTINGS.playerState.ok;
+    this.state = SETTINGS.PLAYER.state.alive;
   }
 
   reset() {
     this.state = SETTINGS.PLAYER.state.alive;
     this.score = 0;
-    this.lives = SETTINGS.PLAYER.score;
+    this.lives = SETTINGS.PLAYER.startLives;
     this.setLives();
     this.setScore();
     this.setHighScore();
-    this.setPosition();
+    this.setPosition(
+      SETTINGS.PLAYER.startPosition.x,
+      SETTINGS.PLAYER.startPosition.y,
+      true
+    );
   }
 
   increFmentScore(amount) {
@@ -44,7 +48,7 @@ class Player extends Sprite {
     if (this.score > this.highScore) {
       this.highScore = this.score;
     }
-    const bestScore = document.querySelector('.score--total');
+    const bestScore = document.querySelector('.counter--best');
     bestScore.textContent = this.highScore;
   }
 }
