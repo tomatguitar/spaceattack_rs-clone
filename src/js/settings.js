3a271503d12b32a24f07d5b5f90d0c836a62c57e
Dmytro Zhyltsov <zkarman@mail.ru>
2021-01-13 14:48:39 +0200

    refactor: change ship's start position

--- src/js/settings.js
+++ src/js/settings.js
@@ -2,20 +2,27 @@ import ship from '../assets/static/images/sship1.png';
 
 const imageFiles = [ship];
 
+const arena = document.querySelector('.game__arena');
+const arenaWidth = parseInt(getComputedStyle(arena).width, 10);
+const arenaHeight = parseInt(getComputedStyle(arena).height, 10);
+const arenaTop = parseInt(arena.getBoundingClientRect().top, 10);
+const arenaBottom = parseInt(arena.getBoundingClientRect().bottom, 10);
+const shipLength = 80; // высота корабля в пикселях
+
 const SETTINGS = {
   CONTROLS: {
-    left: 37,
-    right: 39,
-    up: 38,
-    down: 40,
+    left: 'ArrowLeft',
+    right: 'ArrowRight',
+    up: 'ArrowUp',
+    down: 'ArrowDown',
     space: 32,
     esc: 27,
   },
   PLAYER: {
     divName: 'sprite--player',
     startPosition: {
-      x: 0,
-      y: 0,
+      x: arenaWidth / 2,
+      y: arenaBottom - (arenaTop + shipLength),
     },
     startLives: 3,
     state: {
@@ -23,12 +30,18 @@ const SETTINGS = {
       dead: 1,
       hitFlashing: 2, // Сколько раз корабль моргнет при попадании
     },
+    moveStep: 8, // размер шага перемещения игрока за одно нажатие клавиши(скорость)
+  },
+  ARENA: {
+    width: arenaWidth,
+    height: arenaHeight,
   },
 };
 
 const GameManager = {
   assets: {},
   player: undefined,
+  keys: {},
 };
 
 export { GameManager, imageFiles, SETTINGS };
