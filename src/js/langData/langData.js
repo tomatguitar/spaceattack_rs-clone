9d7e8f670fbc02d42e1f909d5b3bb74e835b8cf7
Dmytro Zhyltsov <zkarman@mail.ru>
2021-01-27 21:19:06 +0200

    feat: update language layouts

---
+++ src/js/langData/langData.js
@@ -0,0 +1,135 @@
+const langData = {
+  languages: {
+    en: {
+      score: {
+        'score-total': 'Score:',
+        'score-best': 'Best:',
+      },
+      stats: {
+        lives: 'Lives:',
+        destroyed: 'Destroyed:',
+        waves: 'Waves:',
+      },
+      menu: {
+        'start-pause': 'Start/Pause',
+        load: 'Load',
+        save: 'Save',
+        settings: 'Settings',
+        about: 'About',
+      },
+      settings: {
+        title: 'Settings',
+        subtitle: {
+          's-lang': 'Language',
+          's-sound': 'Sounds',
+          's-vol': 'Sound volume',
+        },
+        buttons: {
+          'btn-en': 'Eng',
+          'btn-ru': 'Rus',
+          'btn-ukr': 'Ukr',
+          'btn-sound-on': 'On',
+          'btn-sound-off': 'Off',
+          'btn-save': 'Save',
+          'btn-close': 'Close',
+        },
+      },
+      messages: {
+        start: 'Press Start button',
+        'game-over': 'Game Over',
+        victory: 'Victory!',
+        pause: 'Game paused',
+        reset: 'Press Start to start new game',
+        go: 'Go!',
+      },
+    },
+    ru: {
+      score: {
+        'score-total': 'Очки:',
+        'score-best': 'Макс:',
+      },
+      stats: {
+        lives: 'Жизни:',
+        waves: 'Волны:',
+        destroyed: 'Уничтожено:',
+      },
+      menu: {
+        'start-pause': 'Старт/Пауза',
+        load: 'Загр.',
+        save: 'Сохр.',
+        settings: 'Настр.',
+        about: 'Об игре',
+      },
+      settings: {
+        title: 'Настройки',
+        subtitle: {
+          's-lang': 'Язык',
+          's-sound': 'Звуки',
+          's-vol': 'Громкость звука',
+        },
+        buttons: {
+          'btn-en': 'Англ',
+          'btn-ru': 'Рус',
+          'btn-ukr': 'Укр',
+          'btn-sound-on': 'Вкл',
+          'btn-sound-off': 'Выкл',
+          'btn-save': 'Сохр.',
+          'btn-close': 'Закрыть',
+        },
+      },
+      messages: {
+        start: 'Нажмите кнопку Старт',
+        'game-over': 'Конец игры',
+        victory: 'Победа!',
+        pause: 'Игра приостановлена',
+        reset: 'Нажмите Старт для новой игры',
+        go: 'Начали!',
+      },
+    },
+    ukr: {
+      score: {
+        'score-total': 'Бали:',
+        'score-best': 'Макс:',
+      },
+      stats: {
+        lives: 'Життя:',
+        waves: 'Хвилі:',
+        destroyed: 'Знищено:',
+      },
+      menu: {
+        'start-pause': 'Старт/Пауза',
+        load: 'Зав.',
+        save: 'Збер.',
+        settings: 'Налашт.',
+        about: 'Про гру',
+      },
+      settings: {
+        title: 'Налаштування',
+        subtitle: {
+          's-lang': 'Мова',
+          's-sound': 'Звуки',
+          's-vol': 'Гучність звуку',
+        },
+        buttons: {
+          'btn-en': 'Англ',
+          'btn-ru': 'Рос',
+          'btn-ukr': 'Укр',
+          'btn-sound-on': 'Вкл',
+          'btn-sound-off': 'Викл',
+          'btn-save': 'Зберігти',
+          'btn-close': 'Закрити',
+        },
+      },
+      messages: {
+        start: 'Натисніть кнопку Старт',
+        'game-over': 'Кінець гри',
+        victory: 'Перемога!',
+        pause: 'Гра призупинена',
+        reset: 'Натисніть Старт для нової гри',
+        go: 'Почали!',
+      },
+    },
+  },
+};
+
+export default langData;
